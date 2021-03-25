1367b250e86c210f3def35e69dac3a00e62295c4
Jesse Wenger <wengerj@gmail.com>
2021-03-25 18:32:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,7 +101,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10, ID.Min
+      WHERE grade = 10
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
