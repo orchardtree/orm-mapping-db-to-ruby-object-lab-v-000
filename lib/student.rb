2c4481152254e47543acf3afdd14bbaae9f2383b
Jesse Wenger <wengerj@gmail.com>
2021-03-25 18:39:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -103,7 +103,7 @@ class Student
       FROM students
       WHERE grade = 10
       GROUP BY grade
-      HAVING Min(id)
+      HAVING MIN(id)
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
