7f66eae0884891fb5e005b1c8e45c6ad519ad973
Jesse Wenger <wengerj@gmail.com>
2021-03-25 18:12:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,9 +20,9 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-      #SELECT *
-      #FROM students
-      #WHERE name = ?
+      SELECT *
+      FROM students
+      WHERE name = ?
       LIMIT 1
       SQL
     DB[:conn].execute(sql, name).map do |row|
